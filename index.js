3b33fc71116118f8923ebacbce9694e7baddd221
Valdec <valdecir.rocha33@gmail.com>
2020-04-29 20:37:04 -0300

    ..

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ setInterval(function() {
 
 if('serviceWorker' in navigator) {
   navigator.serviceWorker
-           .register('sw.js')
+           .register('rocha33.github.io/sw.js')
            .then(function() { console.log('Service Worker Registered'); });
 }
 
