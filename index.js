8f4a89c78275f49f26c38be98ad4975acf44f3b0
Valdec <valdecir.rocha33@gmail.com>
2020-04-29 22:21:56 -0300

    ....

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const images = ['fox1','fox2','fox3','fox4'];
 const imgElem = document.querySelector('img');
 
-function randomValueFromArray(array) {
-  let randomNo =  Math.floor(Math.random() * array.length);
-  return array[randomNo];
-}
+
 
 
 
@@ -15,10 +12,14 @@ if('serviceWorker' in navigator) {
            .register('sw.js')
            .then(function() { console.log('Service Worker Registered'); });
 }
-setInterval(function() {
-  let randomChoice = randomValueFromArray(images);
-  imgElem.src = 'images/' + randomChoice + '.jpg';
-}, 2000)
+function randomValueFromArray(array) {
+  let randomNo =  Math.floor(Math.random() * array.length);
+  return array[randomNo];
+}
+//setInterval(function() {
+  //let randomChoice = randomValueFromArray(images);
+ // imgElem.src = 'images/' + randomChoice + '.jpg';
+//}, 2000)
 
 // Code to handle install prompt on desktop
 
